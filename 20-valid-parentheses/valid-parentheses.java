e2a99f2338c7e405f11269fd494f8630b522b759
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2025-06-05 22:23:38 +0530

    Time: 3 ms (41.06%) | Memory: 41.7 MB (81.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                char top = stack.isEmpty() ? '#' : stack.pop();
+                if (top != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
