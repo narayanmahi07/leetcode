196951f18bbbf6faa993eb0ae28a364230d76111
Narayan Mahato <122993980+narayanmahi07@users.noreply.github.com>
2025-06-07 19:23:22 +0530

    Time: 3 ms (100.00%) | Memory: 45.3 MB (95.02%) - LeetSync

---
+++ 1739-split-two-strings-to-make-palindrome/split-two-strings-to-make-palindrome.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean checkPalindromeFormation(String a, String b) {
+        return check(a, b) || check(b, a);
+    }
+
+    private boolean check(String a, String b) {
+        int i = 0, j = a.length() - 1;
+
+        while (i < j && a.charAt(i) == b.charAt(j)) {
+            i++;
+            j--;
+        }
+
+        return isPalindrome(a, i, j) || isPalindrome(b, i, j);
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        return true;
+    }
+}
